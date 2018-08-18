bd37f74f8fa564315a2e53085c57313f26d440b2
Nilabhra <nilabhraroy@gmail.com>
2018-08-18 08:59:31 +0200

    Added NALU module (with input independant gating)

---
+++ models/nalu.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn as nn
+
+
+class NALU(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.G = nn.Parameter(torch.Tensor(1, 1))
+        nn.init.xavier_normal_(self.G)
+        self.nac = NAC(self.in_dim, self.out_dim)
+        self.eps = 1e-12
+
+    def forward(self, x):
+        a = self.nac(x)
+        g = torch.sigmoid(self.G)
+        m = self.nac(torch.log(torch.abs(x) + self.eps))
+        m = torch.exp(m)
+        y = (g * a) + (1 - g) * m
+        return y
