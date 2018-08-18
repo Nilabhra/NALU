490b03598488e64f3fde664c0a8020ba2dd50eb4
Nilabhra <nilabhraroy@gmail.com>
2018-08-18 08:57:10 +0200

    NAC module added

---
+++ models/nac.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn as nn
+
+
+class NAC(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.W_hat = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim))
+        self.M_hat = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim))
+        nn.init.xavier_normal_(self.W_hat)
+        nn.init.xavier_normal_(self.M_hat)
+        self.bias = None
+        
+    def forward(self, x):
+        W = torch.tanh(self.W_hat) * torch.sigmoid(self.M_hat)
+        return F.linear(x, W, self.bias)
