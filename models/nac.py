b371fc52fa807a2b8d0b09fde6600956ba140f47
Nilabhra <nilabhraroy@gmail.com>
2018-08-18 09:27:27 +0200

    Fixed imports

--- models/nac.py
+++ models/nac.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn as nn
+from torch.nn import functional as F
 
 
 class NAC(nn.Module):
--- models/nalu.py
+++ models/nalu.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn as nn
+from models.nac import NAC
 
 
 class NALU(nn.Module):
